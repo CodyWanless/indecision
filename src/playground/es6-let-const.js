6f712b3ee918f89fce6770151ab9e1f43a44983f
Wanless <wanlessc@mfldclin.org>
2017-10-30 15:28:48 -0500

    Initial commit

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,23 @@
+// let nameVar = 'Andrew';
+// nameVar = 'Mike';
+
+// console.log('nameVar', nameVar);
+
+// var fullName = 'Cody Wanless';
+
+// if (fullName) {
+//     const firstName = fullName.split(' ')[0];
+//     console.log(firstName);
+// }
+
+// console.log(firstName);
+
+// const multiplier = {
+//     numbers: [3,6],
+//     multiplyBy: 5,
+//     multiply() {
+//         return numbers.map(x => x * this.multiplyBy);
+//     }
+// };
+
+// console.log(multiplier.multiply());
