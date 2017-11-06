e515bb56cc01a644b021d57c2afdf2904361fd2d
Cody Wanless <codywanless@Codys-MacBook-Pro.local>
2017-11-06 17:29:11 -0600

    Prep for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,48 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const publicPath = path.join(__dirname, 'public');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: publicPath,
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{ // run babel when a js file is encountered outside of node_modules/
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ],
-            test: /\.s?css$/
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: publicPath
-    }
+module.exports = (env, argv) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry: ['babel-polyfill', './src/app.js'],
+        output: {
+            path: publicPath,
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{ // run babel when a js file is encountered outside of node_modules/
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                use: CSSExtract.extract({
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }]
+                }),
+                test: /\.s?css$/
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: publicPath,
+            historyApiFallback: true,
+            publicPath: '/'
+        }
+    };
 };
