f187b4df663f690ab46ba08643ce4d69135daaf2
Cody Wanless <codywanless@gmail.com>
2022-11-20 12:35:10 -0600

    fix lingering warnings and test code from yesteryear

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss'
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<IndecisionApp />);
