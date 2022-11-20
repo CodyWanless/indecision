3b5921166b84762b41a3581eef6185d6080bf1ae
Cody Wanless <CodyWanless@users.noreply.github.com>
2022-11-20 12:36:31 -0600

    No free lunch (#1)
    * rm build targets from repo
    
    * update all of the dependencies
    
    * fix lingering warnings and test code from yesteryear

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import IndecisionApp from './components/IndecisionApp';
-import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<IndecisionApp />);
--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -8,7 +8,8 @@ const OptionModal = (props) => (
         onRequestClose={props.handleClearSelectedOption} // this needs to be set in order for escape/clicking on background to close the modal
         closeTimeoutMS={200}
         className="modal"
-        >
+        appElement={document.getElementById('app')}
+    >
         <h3 className="modal__title">Selected Option</h3>
         {(props.selectedOption && <p className="modal__body">{props.selectedOption}</p>)}
         <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
